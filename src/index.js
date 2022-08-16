d27ff738e72bb888ff9b84e61ea38187f281d6c8
Zablon Dawit <zdawit@pelmorex.com>
2022-08-17 01:47:13 +0300

    feat: add main feature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log("Hello, World");
+const showdown = require("showdown");
+const converter = new showdown.Converter();
+
+const onReadyListener = () => {
+  /** @type {HTMLTextAreaElement} */
+  const ideaElem = document.getElementById("idea");
+  const brainstormedElem = document.getElementById("brainstormed");
+  let shiftModPressed = false;
+  
+  if (!ideaElem) return;
+  if (!brainstormedElem) return;
+
+  ideaElem.addEventListener("keydown", (e) => {
+    if (e.key !== "Shift") return;
+    if (e.repeat) return;
+
+    shiftModPressed = true;
+  })
+
+  ideaElem.addEventListener("keyup", (e) => {
+    if (e.key !== "Shift") return;
+    shiftModPressed = false;
+  })
+
+  ideaElem.addEventListener("keydown", (e) => {
+    if (e.key !== "Enter") return;
+    if (!shiftModPressed) return;
+    
+    const ideaText = ideaElem.value;
+    const ideaHtml = converter.makeHtml(ideaText);
+
+    if (brainstormedElem.children.length > 0)
+      brainstormedElem.prepend(document.createElement("hr"));
+
+    const newIdeaElem = document.createElement("div");
+    newIdeaElem.innerHTML = ideaHtml;
+    brainstormedElem.prepend(newIdeaElem);
+
+    e.preventDefault();
+  })
+}
+
+document.addEventListener('DOMContentLoaded', onReadyListener);
